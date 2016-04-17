48dee5a7f61f5a2cb24ad9f8b048e8362120a470
André Lage <prof.alage@gmail.com>
2016-04-17 16:46:43 -0300

    replaced by the right file

--- SimpleConvolution.jl
+++ SimpleConvolution.jl
@@ -1,35 +1,48 @@
-@printf "adding 2 processes (Workers)\n"
-addprocs(2)
+# Example of a simple convolution operation using DistributedArrays.
+# Author: André Lage-Freitas
 
-@printf "using DistributedArrays package\n"
+n_of_workers=4
+darray_size=10
+
+# removing previous Workers if they exist
+if nprocs() > 1
+	rmprocs(workers())
+end
+
+println("adding $n_of_workers Workers")
+addprocs(n_of_workers)
+@show workers()
+
+# using DistributedArrays package
 using DistributedArrays
 
-@printf "define at all processes (Master and Workers) the ImageData type\n"
+println("defining at all processes (Master and Workers) the ImageData type")
 @everywhere type ImageData
-	raw_data # polSAR image data itself
-	processed_data
+    # typing to Float64 is not necessary but increase performance
+    raw_data::Float64 # polSAR image data itself
+    processed_data::Float64
 end
 
-
-
-@printf "create a DistributedArray with image data\n"
-@printf "DistributedArray data is automatic stored at Workers\n"
-@printf "remark: CloudArray allows to create DistributedArrays from files\n"
+println("creating a DistributedArray with a mock image")
+println("  create a $(darray_size)-dimension mock image by using ImageData")
+array = []
+for i=1:10
+	push!(array, ImageData(rand(), 0.0))
+end
+println("  turning the array into a DArray")
+# DistributedArray data is automatic created and stored at Workers
+# remark: CloudArray allows to create DistributedArrays from files
+#darray = dfill(ImageData(rand(), 0.0), darray_size) # dimension is darray_size
 darray = distribute(array)
 
-@printf "for all workers\n"
+# for all workers
 for w in workers()
-	# iterates over darray data stored at Worker w (this is done remotely, at Worker w)
-	@spawnat w for i=2:length(localpart(darray))-1 # localpart(darray) means the darray data stored at Worker w
-		# calculate a simple convolution (BUGFIX address overlap areas)
-		localpart(darray)[i].processed_data = ( localpart(darray)[i-1].raw_data + localpart(darray)[i].raw_data + localpart(darray)[i+1].raw_data ) / 3
-	end
+    # iterates over darray data stored at Worker w (this is done remotely, at Worker w)
+    @spawnat w for i=2:length(localpart(darray))-1 # localpart(darray) means the darray data stored at Worker w
+        # calculate a simple convolution (BUGFIX address overlap areas)
+        localpart(darray)[i].processed_data = ( localpart(darray)[i-1].raw_data + localpart(darray)[i].raw_data + localpart(darray)[i+1].raw_data ) / 3
+    end
 end
 
-@printf "printing the image and the convolution\n"
-for i=1:length(darray)
-	@show darray[i]
-end 
-
-#@printf "sleeping 10 secs to wait Workers finish their jobs...\n"
-#sleep(10)
+println("Printing the image and its convolution")
+for i=1:length(darray) @show darray[i]; end 
